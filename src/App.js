f08a270152f106c6821217240ecfe7b937ee6348
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-07-25 16:02:16 -0600

    Import preconfig styles and pizza data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+
+import { Container, Row, Col } from "react-bootstrap";
+import { PizzaCard } from "./components/PizzaCard";
+import { Confirmation } from "./components/Confirmation";
+import pizzas from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <p>Hello There</p>
+      </Row>
+    </Container>
   );
 }
 
